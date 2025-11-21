708ac780c19ace389420891cd3ff330b35916d5e
AbhishekOjha0789 <abhishekojha0789@gmail.com>
2025-11-21 06:26:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,31 +1,18 @@
 import express from "express";
 import cors from "cors";
-import dotenv from "dotenv";
-import { connectDB } from "./config/db.js";
+import connectDB from "./config/db.js";
 import userRoutes from "./routes/userRoutes.js";
 
-dotenv.config();
-connectDB();
-
 const app = express();
+
 app.use(cors());
 app.use(express.json());
+connectDB();
+
+app.get("/", (req, res) => res.send("Backend is Running"));
 
 app.use("/", userRoutes);
 
-app.listen(process.env.PORT || 4000, () =>
-  console.log("Server running")
+app.listen(process.env.PORT || 3000, () =>
+  console.log("Server running...")
 );
-app.get("/", (req, res) => {
-  res.send("Server Running");
-});
-
-// Check MongoDB status
-app.get("/test-db", async (req, res) => {
-  try {
-    await mongoose.connection.db.admin().ping();
-    res.json({ status: "ok", mongo: "connected" });
-  } catch (e) {
-    res.json({ status: "error", mongo: e.message });
-  }
-});
