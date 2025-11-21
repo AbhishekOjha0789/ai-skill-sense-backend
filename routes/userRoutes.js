3bb83e11e84f92f49a34f692fb596fca16823564
AbhishekOjha0789 <abhishekojha0789@gmail.com>
2025-11-21 06:19:30 +0530

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,18 +1,36 @@
 import express from "express";
-import {
-  signup,
-  login,
-  getUser,
-  syncPartial,
-  syncUser
-} from "../controllers/userController.js";
+import User from "../models/User.js";
 
 const router = express.Router();
 
-router.post("/signup", signup);
-router.post("/login", login);
-router.post("/getUser", getUser);
-router.post("/syncPartial", syncPartial);
-router.post("/syncUser", syncUser);
+router.post("/login", async (req, res) => {
+  const { uid, password } = req.body;
+
+  if (!uid || !password)
+    return res.json({ success: false, message: "Missing fields" });
+
+  let user = await User.findOne({ uid });
+
+  if (!user)
+    return res.json({ success: false, message: "User not found" });
+
+  if (user.password !== password)
+    return res.json({ success: false, message: "Wrong password" });
+
+  res.json({ success: true, user });
+});
+
+router.post("/syncUser", async (req, res) => {
+  const data = req.body;
+  const { uid } = data;
+
+  let user = await User.findOneAndUpdate(
+    { uid },
+    { $set: data },
+    { new: true, upsert: true }
+  );
+
+  res.json({ success: true, message: "User synced successfully", user });
+});
 
 export default router;
