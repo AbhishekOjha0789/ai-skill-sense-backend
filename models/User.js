8b098bd0d1e89131d904d7f012c9fc682b180210
AbhishekOjha0789 <abhishekojha0789@gmail.com>
2025-11-21 06:42:27 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,10 +1,15 @@
-import mongoose from "mongoose";
-
 const UserSchema = new mongoose.Schema({
-  uid: String,
-  password: String,
-  personal_info: Object,
-  skills: Array
-});
 
-export default mongoose.model("User", UserSchema);
+    uid: { type: String, required: true, unique: true },
+    password: String,
+
+    users: Object,             // table: users
+    personal_info: Object,     // table: personal_info
+    professional_info: Object, // table: professional_info
+    social_info: Object,       // table: social_info
+    user_points: Object,       // table: user_points
+    
+    skills: Array,             // table: skills
+    quest_log: Array,          // table: quest_log
+    settings: Object           // table: settings
+});
