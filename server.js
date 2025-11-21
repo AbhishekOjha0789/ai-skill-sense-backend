4de99fd3376f685b3b7cb163501ef7829c6283f4
AbhishekOjha0789 <abhishekojha0789@gmail.com>
2025-11-21 07:38:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get("/", (req, res) => res.send("Backend is Running"));
 
 app.use("/", userRoutes);
 
-port = process.env.PORT || 4000;
+const port = process.env.PORT || 4000;
 
 app.listen(port, () =>
   console.log("Server running on port", port)
